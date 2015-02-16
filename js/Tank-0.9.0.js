3b945a4d4db62f144f168aa58223d7015ab5f3fd
gviggiano <giuseppe.viggiano@gmail.com>
2015-02-16 14:12:54 +0100

    some css layout beautification

--- js/Tank-0.9.0.js
+++ js/Tank-0.9.0.js
@@ -63,7 +63,8 @@ window.Tank = function () {
     };
 
     var forEach = function (array, fn) {
-        [].forEach.call(Tank.toArray(array), fn);
+        var cb = Array.prototype.slice.call(arguments,[arguments.length-1]);
+        [].forEach.call(Tank.toArray(array), cb[0]);
     };
 
     var model = function (expression, handler) {
@@ -269,7 +270,6 @@ window.Tank = function () {
                 append: function (Element, Selector) {
                     var scriptElements = [];
                     Tank.forEach(Element, function (elem) {
-                        //console.log(elem);
                         Tank.forEach(Tank.element(Selector), function (selector) {
                             if (elem.nodeType !== 3 && elem.querySelectorAll) {
                                 var scripts = Tank.element('script', elem);
@@ -353,16 +353,14 @@ window.Tank = function () {
                  @param Styles (  { name: string, attrs: Object | Array.<{name: string, attrs: Object}> )
                  */
                 styledef: function (Styles) {
-                    var str = '';
-                    Tank.forEach(Styles, function (style) {
-                        str += style.name.concat("{\n"+Object.keys(style.attrs).map(function (el) {
-                            return "\t".concat(el, ':', style.attrs[el], ';\n');
-                        }).join(''),"}\n");
-                    });
-                    var href = Tank.format('data:text/css;base64,{0}', window.btoa(str));
                     this.flow = {
                         append: {
-                            Element: Tank.createElement('link', {rel: "stylesheet", href: href, type: "text/css", async: false}),
+                            Element: Tank.createElement('link', {rel: "stylesheet",
+                                href: Tank.format('data:text/css;base64,{0}', window.btoa(Tank.toArray(Styles).map(function (style) {
+                                    return style.name.concat("{\n"+Object.keys(style.attrs).map(function (el) {
+                                        return "\t".concat(el, ':', style.attrs[el], ';\n');
+                                    }).join(''),"}\n");
+                            }).join(''))), type: "text/css", async: false}),
                             Selector: "head", exec: this.copy(this.flow)
                         }
                     };
